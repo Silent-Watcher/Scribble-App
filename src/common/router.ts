eca4900ff4ecd061be66524955f6da1e8c278a0b
Silent-Watcher <backendwithali@gmail.com>
2024-09-28 16:56:02 +0330

    feat: add /health route and complete its document inside swagger doc config

--- src/common/router.ts
+++ src/common/router.ts
@@ -1,5 +1,6 @@
 import authRouter from '$modules/auth/auth.routes';
 import { Router } from 'express';
+import httpStatus from 'http-status';
 
 import type { Application, NextFunction, Request, Response } from 'express';
 const router = Router();
@@ -17,6 +18,19 @@ router.use('/auth', authRouter);
 router.get('/about', (_req: Request, res: Response, next: NextFunction) => {
   try {
     res.render('about');
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/health', (_req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.status(httpStatus.OK).send({
+      status: res.statusCode,
+      code: 'OK',
+      message: 'server is up and running ...',
+    });
+    return;
   } catch (error) {
     next(error);
   }
