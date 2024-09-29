22fbdae19630019398214abdbf83899dbad1bf82
Silent-Watcher <backendwithali@gmail.com>
2024-09-30 02:12:15 +0330

    refactor: add gitattribute file for handling image files

--- src/primary.ts
+++ src/primary.ts
@@ -4,14 +4,12 @@ import path from 'path';
 
 const cpuLength = os.cpus().length;
 
-
 cluster.setupPrimary({
-	exec : path.join(process.cwd() , 'src', 'common' , 'bootstrap.ts')
-})
-
+  exec: path.join(process.cwd(), 'src', 'common', 'bootstrap.ts'),
+});
 
 for (let index = 0; index < cpuLength; index++) {
-	cluster.fork();
+  cluster.fork();
 }
 
-cluster.on('exit', ()=>cluster.fork())
+cluster.on('exit', () => cluster.fork());
